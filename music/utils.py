2ba345210a1ccd9078f25359db22a0a3c9ff3fa2
J Miller <junyatwin@gmail.com>
2026-06-08 10:33:55 -0700

    starting over completely

--- music/utils.py
+++
@@ -1,6 +0,0 @@
-def create_bar(current: int, total: int, length: int = 12):
-    if not total:
-        return "▱" * length
-
-    progress = int((current / total) * length)
-    return "▰" * progress + "▱" * (length - progress)
